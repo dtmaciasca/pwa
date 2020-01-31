1d2d5cdf7b8ae0a4f9af9a9557c6d233630f94bb
Glitch (dtmaciasca-pwa) <none>
2020-01-31 02:34:37 +0000

    🎯🐊 Checkpoint ./scripts/sw.js:10783751/552 ./index.html:10783751/18

--- sw.js
+++ sw.js
@@ -2,8 +2,13 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
+
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-  workbox.precaching.precacheAndRoute([]);
+  workbox.precaching.precacheAndRoute([
+    { url: "/index.html", revision:"383676"},
+    { url: "/styles/inline.css", revision:null},
+    { url: "/scripts/app.js", revision:"383676"},
+  ]);
